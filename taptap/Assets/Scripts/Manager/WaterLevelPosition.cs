b68399fba0145af31f21db5765bd1463f66d194e
my <my358676932@gmail.com>
2020-12-24 19:01:25 +0800

    waterSystem

---
+++ taptap/Assets/Scripts/Manager/WaterLevelPosition.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum WaterType
+{
+    PureWater,
+    HotWater,
+    PoolWater,
+}
+
+
+
+public class WaterLevelPosition : MonoBehaviour
+{
+    public WaterManager waterManager;
+    public WaterType waterType;
+    public float TransformScale = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (waterType)
+        {
+            case WaterType.PureWater:
+                transform.position = new Vector3(transform.position.x,
+                                                waterManager.CurrentWaterPurifierWaterLevel/TransformScale,
+                                                transform.position.z);
+            break;
+
+            case WaterType.HotWater:
+                transform.position = new Vector3(transform.position.x,
+                                                waterManager.CurrentBoilerWaterLevel/TransformScale,
+                                                transform.position.z);
+            break;
+
+            //case WaterType.PoolWater:
+            //    transform.position = new Vector3(transform.position.x,
+            //                                    waterManager.CurrentWaterPurifierWaterLevel,
+            //                                    transform.position.z);
+            //break;
+
+        }
+    }
+}
---
+++ taptap/Assets/Scripts/Manager/WaterManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterManager : MonoBehaviour
+{
+    [Space(5)]
+    public float MaxDirtyWaterLevel = 100;
+    public float CurrentDirtyWaterLevel;
+    [Space(5)]
+    public float MaxWaterPurifierWaterLevel = 100;
+    public float CurrentWaterPurifierWaterLevel;
+    [Space(5)]
+    public float MaxBoilerWaterLevel = 100;
+    public float CurrentBoilerWaterLevel;
+    [Space(5)]
+    public float OutputWaterTemperature;
+    public float RoomTemperature = 26;
+    [Space(5)]
+    public float HeatingSpeed = 5;
+    public float CoolDownSpeed = -1;
+    [Space(5)]
+    public float InputSpeed = 10;
+    public float PuringSpeed = 5;
+    public float TransportingSpeed = 10;
+    public float OutputSpeed = 5;
+    [Space(5)]
+    public bool isHeating;
+    public bool isPuring;
+    public bool isInPuting;
+    public bool isTransporting;
+    public bool isOutPuting;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        WaterLevelProcessing(isInPuting, ref CurrentDirtyWaterLevel, MaxDirtyWaterLevel, InputSpeed);
+        
+        WaterLevelTransporting(isPuring, ref CurrentWaterPurifierWaterLevel, MaxWaterPurifierWaterLevel, ref CurrentDirtyWaterLevel, PuringSpeed);
+
+        WaterLevelTransporting(isTransporting, ref CurrentBoilerWaterLevel, MaxBoilerWaterLevel, ref CurrentWaterPurifierWaterLevel, TransportingSpeed);
+
+        WaterTemperatureProcessing(true, CurrentBoilerWaterLevel, ref OutputWaterTemperature, CoolDownSpeed,RoomTemperature);
+        WaterTemperatureProcessing(isHeating, CurrentBoilerWaterLevel, ref OutputWaterTemperature, HeatingSpeed,0);
+
+        WaterLevelProcessing(isOutPuting, ref CurrentBoilerWaterLevel, MaxBoilerWaterLevel, OutputSpeed);
+        Debug.Log(CurrentWaterPurifierWaterLevel);
+    }
+
+    void WaterOutPut()
+    {
+        if(CurrentBoilerWaterLevel > 0)
+        {
+            CurrentBoilerWaterLevel -= OutputSpeed * Time.deltaTime;
+        }
+    }
+    
+    void WaterLevelProcessing(bool type, ref float level, float maxLevel, float speed)
+    {
+        if(type &&  level >= 0 && level < maxLevel)
+        {
+            level += speed * Time.deltaTime;
+        }
+
+    }
+
+    void WaterLevelTransporting(bool type, ref float level, float maxLevel, ref float minusLevel, float speed)
+    {
+        if(type &&  minusLevel >= 0 && level < maxLevel)
+        {
+            level += speed * Time.deltaTime;
+            minusLevel -= speed * Time.deltaTime;
+        }
+    }
+
+    void WaterTemperatureProcessing(bool type, float level, ref float temperature, float speed, float envTemperature)
+    {
+        if(type && level > 0 && temperature >= envTemperature && temperature <= 100)
+        {
+            temperature += speed * Time.deltaTime;
+        }
+    }
+
+}
